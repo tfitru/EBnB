604e6413f894d259c4847aa93047efe2c75154be
Fitru <fitru@Zipcoders-MacBook-Pro-100.local>
2022-09-09 11:47:13 -0400

    working repo

--- src/main/java/EBNB/Ebnb/controllers/ShelterController.java
+++ src/main/java/EBNB/Ebnb/controllers/ShelterController.java
@@ -3,6 +3,8 @@ package EBNB.Ebnb.controllers;
 import EBNB.Ebnb.Repo.RepoShelter;
 import EBNB.Ebnb.models.Shelter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ShelterController {
 
     @Autowired
-    RepoShelter
+    RepoShelter repoShelter;
 
     @PostMapping
     public ResponseEntity<Shelter> createShelter(@RequestBody Shelter shelter){
+        shelter = repoShelter.save(shelter);
+        return new ResponseEntity<>(shelter, HttpStatus.CREATED);
 
     }
 
